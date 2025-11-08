8c3a7d2e7d011c0638ce802cc338a0c02ec458c7
everdro1d <everdro1d-github@pm.me>
2025-11-08 02:21:03 -0700

    update trouble v3

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,15 +1,15 @@
-require("trouble").setup({
-    icons = true,
-})
+local trouble = require("trouble")
+
+trouble.setup({})
 
 vim.keymap.set("n", "<leader>tt", function()
-    require("trouble").toggle()
+    trouble.toggle("diagnostics")
 end)
 
 vim.keymap.set("n", "[t", function()
-    require("trouble").next({skip_groups = true, jump = true});
+    trouble.next({skip_groups = true, jump = true}, "diagnostics");
 end)
 
 vim.keymap.set("n", "]t", function()
-    require("trouble").previous({skip_groups = true, jump = true});
+    trouble.previous({skip_groups = true, jump = true}, "diagnostics");
 end)
