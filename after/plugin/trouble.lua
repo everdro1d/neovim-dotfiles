37f4941e0e2cb8e0b3ee434212ae5c5144c8ed21
everdro1d <everdro1d-github@pm.me>
2025-11-08 02:23:59 -0700

    update trouble keybind desc

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -4,12 +4,12 @@ trouble.setup({})
 
 vim.keymap.set("n", "<leader>tt", function()
     trouble.toggle("diagnostics")
-end)
+end, { desc = "toggle trouble diag. buffer" })
 
 vim.keymap.set("n", "[t", function()
     trouble.next({skip_groups = true, jump = true}, "diagnostics");
-end)
+end, { desc = "goto next diag. trouble item" })
 
 vim.keymap.set("n", "]t", function()
     trouble.previous({skip_groups = true, jump = true}, "diagnostics");
-end)
+end, { desc = "goto prev. diag. trouble item" })
