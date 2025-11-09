4ef604f1c5a36e5e9dc088e0d6782bf70c1c40b6
everdro1d <everdro1d-github@pm.me>
2025-11-08 19:34:50 -0700

    add line numbers to netrw

--- lua/everdro1d/core/init.lua
+++ lua/everdro1d/core/init.lua
@@ -31,6 +31,7 @@ autocmd({"BufWritePre"}, {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
 
 -- remap mappings for netrw
 autocmd('filetype', {
