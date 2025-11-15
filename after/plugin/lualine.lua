e5fc443c87b58b8ed4bcc4aa2867d7dc73d202ca
everdro1d <everdro1d-github@pm.me>
2025-11-14 22:48:21 -0700

    lualine: rm filetype icon

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -37,7 +37,7 @@ ll.setup {
       lualine_a = { { 'mode', fmt = function(str) return str:sub(1,1) end } },
       lualine_b = {'branch', 'diff', 'diagnostics'},
       lualine_c = {'filename'},
-      lualine_x = {'encoding', { 'filetype', icon = nil } },
+      lualine_x = {'encoding', { 'filetype', icon = '' } },
       lualine_y = {'progress'},
       lualine_z = {'location'}
     },
