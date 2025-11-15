05bfc5b70ce979edc407690d0bfea8eb0c76ef71
everdro1d <everdro1d-github@pm.me>
2025-11-14 22:17:57 -0700

    lualine: format

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -4,8 +4,6 @@ ll.setup {
     options = {
       icons_enabled = true,
       theme = 'auto',
-      -- component_separators = { left = '', right = ''},
-      -- section_separators = { left = '', right = ''},
       component_separators = '',
       section_separators = '',
       disabled_filetypes = {
@@ -36,10 +34,10 @@ ll.setup {
       }
     },
     sections = {
-      lualine_a = {'mode'},
+      lualine_a = { { 'mode', fmt = function(str) return str:sub(1,1) end } },
       lualine_b = {'branch', 'diff', 'diagnostics'},
       lualine_c = {'filename'},
-      lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_x = {'encoding', { 'filetype', icon = nil } },
       lualine_y = {'progress'},
       lualine_z = {'location'}
     },
