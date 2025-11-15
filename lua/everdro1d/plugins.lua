78a9b46add6f01cade6d309ae9202a932aacd137
everdro1d <everdro1d-github@pm.me>
2025-11-14 21:31:00 -0700

    add lualine

--- lua/everdro1d/plugins.lua
+++ lua/everdro1d/plugins.lua
@@ -63,4 +63,12 @@ return {
     {
         "folke/trouble.nvim",
     },
+    {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {
+        "nvim-tree/nvim-web-devicons",
+        opts = {}
+    },
 }
