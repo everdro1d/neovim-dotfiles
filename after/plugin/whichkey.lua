6e35736503708818814c463e702c0038821fef8b
everdro1d <everdro1d-github@pm.me>
2026-05-03 21:25:38 -0600

    whichkey: organize key hints

--- after/plugin/whichkey.lua
+++ after/plugin/whichkey.lua
@@ -1,18 +1,23 @@
 local wk = require("which-key")
 
 wk.add({
+    { "<leader>es", desc = "view file system" },
+
+    { "<C>p", desc = "find git file (tele.)" },
+
+    { "<leader>a", desc = "add a file to Harpoon" },
+    { "<C>e", desc = "open/close Harpoon menu" },
     { "<C>dH", desc = "switch to next buffer in Harpoon" },
     { "<C>dS", desc = "switch to prev. buffer in Harpoon" },
-    { "<C>e", desc = "open/close Harpoon menu" },
     { "<C>h", desc = "switch to Harpoon 1" },
-    { "<C>n", desc = "switch to Harpoon 3" },
-    { "<C>p", desc = "find git file (tele.)" },
-    { "<C>s", desc = "switch to Harpoon 4" },
     { "<C>t", desc = "switch to Harpoon 2" },
-    { "<leader>a", desc = "add a file to Harpoon" },
+    { "<C>n", desc = "switch to Harpoon 3" },
+    { "<C>s", desc = "switch to Harpoon 4" },
+
     { "<leader>n", group = "neogen" },
     { "<leader>nf", desc = "function" },
     { "<leader>nt", desc = "type" },
+
     { "<leader>p", group = "in project" },
     { "<leader>pW", desc = "find with grep WORD (vim.opt)" },
     { "<leader>pWs", desc = "find with grep WORD" },
@@ -20,14 +25,18 @@ wk.add({
     { "<leader>ps", desc = "find with grep string" },
     { "<leader>pw", desc = "find with grep word (vim)" },
     { "<leader>pws", desc = "find with grep WORD" },
-    { "<leader>es", desc = "view file system" },
+
+
     { "<leader>tt", desc = "toggle trouble buffer" },
-    { "<leader>u", desc = "show/hide undo tree" },
+
     { "<leader>v", group = "buffers" },
     { "<leader>vca", desc = "code actions" },
     { "<leader>vrn", desc = "rename" },
     { "<leader>vrr", desc = "references" },
+
+    { "<leader>u", desc = "show/hide undo tree" },
     { "<tab>", group = "switch focus editor & undo tree" },
+
     { "K", desc = "show hover docs." },
     { "gd", desc = "go to definition" },
 })
