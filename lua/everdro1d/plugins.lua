1b0e4915bb4a94934326dcc2f8f1d580784a0842
everdro1d <everdro1d-github@pm.me>
2026-06-20 01:23:51 -0600

    refactor: organize the plugin listing into categories

--- lua/everdro1d/plugins.lua
+++ lua/everdro1d/plugins.lua
@@ -1,10 +1,54 @@
 return {
+
+
+    -- Cosmetic --
     {
         "rebelot/kanagawa.nvim",
     },
     {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {
+        'brenoprata10/nvim-highlight-colors'
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        ---@module "ibl"
+        ---@type ibl.config
+        opts = {},
+    },
+    {
+        "HiPhish/rainbow-delimiters.nvim",
+    },
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+	},
+
+
+    -- Functional --
+    {
         "mbbill/undotree",
     },
+    {
+        "folke/trouble.nvim",
+    },
+    {
+        'andymass/vim-matchup'
+    },
+    {
+        "kylechui/nvim-surround",
+        event = "VeryLazy",
+    },
+
+
+    -- Navigation --
 	{
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
@@ -24,14 +68,9 @@ return {
 	{
 		"mfussenegger/nvim-dap",
 	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-	},
+
+
+    -- LSP, Tree-sitter, code-generation, etc. --
     {
         "romus204/tree-sitter-manager.nvim",
     },
@@ -41,40 +80,13 @@ return {
             "neovim/nvim-lspconfig",
             "j-hui/fidget.nvim",
         },
-		enabled = function()
-			-- disable on windows due to nix dependency
-			return vim.fn.has("win32") == 0 and vim.fn.has("win64") == 0
-		end,
+        enabled = function()
+            -- disable on windows due to nix dependency
+            return vim.fn.has("win32") == 0 and vim.fn.has("win64") == 0
+        end,
     },
     {
         "danymat/neogen",
         version = "*",
-    },
-    {
-        "folke/trouble.nvim",
-    },
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' }
-    },
-    {
-        'brenoprata10/nvim-highlight-colors'
-    },
-    {
-        'andymass/vim-matchup'
-    },
-    {
-        "kylechui/nvim-surround",
-        event = "VeryLazy",
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
-        opts = {},
-    },
-    {
-        "HiPhish/rainbow-delimiters.nvim",
     },
 }
