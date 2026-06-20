e63ab1105860221869afac01e522a48295e40458
everdro1d <everdro1d-github@pm.me>
2026-06-20 01:13:19 -0600

    fix: autocomplete no longer appears in temp buffers (like telescope)

--- lua/everdro1d/core/modules/native-cmp.lua
+++ lua/everdro1d/core/modules/native-cmp.lua
@@ -3,3 +3,10 @@ vim.o.autocompletedelay = 0
 vim.o.pumheight = 7
 vim.o.pumborder = "rounded"
 vim.opt.completeopt = { "menuone", "noinsert", "popup" }
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    group = vim.api.nvim_create_augroup("autocompletion-sanitizer", { clear = true }),
+    callback = function(ev)
+        if vim.bo[ev.buf].buftype ~= "" then vim.bo[ev.buf].autocomplete = false end
+    end,
+})
