310e89ad7288f6d69b2450872494203a9314871b
everdro1d <everdro1d-github@pm.me>
2026-07-03 00:22:51 -0600

    feat: update statuscolumn to %l%s

--- lua/everdro1d/core/set.lua
+++ lua/everdro1d/core/set.lua
@@ -1,6 +1,10 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.statuscolumn = "%l%s"
+vim.opt.numberwidth = 3
+vim.opt.signcolumn = "yes:1"
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
