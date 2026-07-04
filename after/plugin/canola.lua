be01d24bc1c766d88b815f3cd616d49831a3cacb
everdro1d <everdro1d-github@pm.me>
2026-07-04 00:40:21 -0600

    feat(canola): add autocmd to write templates for new files

--- after/plugin/canola.lua
+++ after/plugin/canola.lua
@@ -1,3 +1,19 @@
+-- write into new files with (ext) by default
+vim.api.nvim_create_autocmd("User", {
+    pattern = "CanolaFileCreated",
+    callback = function(args)
+        local path = args.data.path
+        local ext = vim.fn.fnamemodify(path, ":e")
+        local templates = {
+            sh = { "#!/usr/bin/env bash", "" },
+            py = { "#!/usr/bin/env python3", "" },
+        }
+        if templates[ext] then
+            vim.fn.writefile(templates[ext], path)
+        end
+    end,
+})
+
 -- refresh after :! shell cmds
 vim.api.nvim_create_autocmd('ShellCmdPost', {
     callback = function()
