30e80c599d0ef0924c7d22a227cda37a5f3575f9
everdro1d <everdro1d-github@pm.me>
2026-07-04 00:43:21 -0600

    feat(canola): add git status to canola sign column

--- after/plugin/canola.lua
+++ after/plugin/canola.lua
@@ -33,3 +33,31 @@ vim.api.nvim_create_autocmd('ShellCmdPost', {
         require('canola-git').invalidate()
     end,
 })
+
+-- git file status in sign column
+local gsns = vim.api.nvim_create_namespace('canola_git_signs')
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'CanolaReadPost',
+    callback = function(args)
+        local bufnr = args.data.buf
+        local canola = require('canola')
+        local cg = require('canola-git')
+        local dir = canola.get_current_dir(bufnr)
+        if not dir then
+            return
+        end
+        vim.api.nvim_buf_clear_namespace(bufnr, gsns, 0, -1)
+        for lnum = 1, args.data.entry_count do
+            local entry = canola.get_entry_on_line(bufnr, lnum)
+            if entry then
+                local st = cg.get_status(dir, entry.name)
+                if st then
+                    vim.api.nvim_buf_set_extmark(bufnr, gsns, lnum - 1, 0, {
+                        sign_text = st.char,
+                        sign_hl_group = st.hl,
+                    })
+                end
+            end
+        end
+    end,
+})
