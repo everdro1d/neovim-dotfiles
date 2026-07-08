6566db79fe6315005337bf4be47427cf40e3da7b
everdro1d <everdro1d-github@pm.me>
2026-07-08 01:51:06 -0600

    feat(plugin): add satellite.nvim

---
+++ lua/everdro1d/plugins/cosmetic/satellite.lua
@@ -0,0 +1,33 @@
+return {
+    "lewis6991/satellite.nvim",
+    event = "VeryLazy",
+    opts = {
+        current_only = true,
+        excluded_filetypes = {
+            'canola',
+            'undotree',
+            'NeogitStatus',
+            'NeogitDiffView',
+            'gitcommit',
+            'codediff-explorer',
+            'TelescopePrompt',
+            'grapple',
+            ''
+        },
+        handlers = {
+            gitsigns = {
+                enable = true,
+                signs = {
+                    add = "+",
+                    change = "~",
+                    delete = "-",
+                },
+            },
+
+            marks = {
+                enable = true,
+                key = '' -- disable mgmt
+            },
+        }
+    }
+}
