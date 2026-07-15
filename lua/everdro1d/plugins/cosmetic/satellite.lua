bbd692b67bfe83a768ff96545469aa9dc0dcf6ea
everdro1d <everdro1d-github@pm.me>
2026-07-15 00:53:44 -0600

    fix(satellite): set marks key to nil - when '' it defaults to 'g'

--- lua/everdro1d/plugins/cosmetic/satellite.lua
+++ lua/everdro1d/plugins/cosmetic/satellite.lua
@@ -26,7 +26,7 @@ return {
 
             marks = {
                 enable = true,
-                key = '' -- disable mgmt
+                key = nil -- disable mgmt
             },
         }
     }
