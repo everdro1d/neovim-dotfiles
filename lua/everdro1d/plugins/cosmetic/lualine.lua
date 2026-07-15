77accf4532f3e1749d4f6147e8a577e36a6b0c6e
everdro1d <everdro1d-github@pm.me>
2026-07-14 23:50:17 -0600

    feat(lualine): add custom searchcount and std selectioncount

--- lua/everdro1d/plugins/cosmetic/lualine.lua
+++ lua/everdro1d/plugins/cosmetic/lualine.lua
@@ -6,6 +6,60 @@ local function get_canola_dir()
         return nil
     end
 end
+
+local last_pattern, last_match_idx, last_interaction_time
+local function search_count()
+    local M = require('lualine.component'):extend()
+    local default_options = {
+        maxcount = 999,
+        timeout = 500,
+        display_timeout = 4000,
+    }
+
+    function M:init(options)
+        M.super.init(self,options)
+        self.options = vim.tbl_extend('keep', self.options or {}, default_options)
+
+    end
+
+    local function update_hl(current, timeout)
+        local current_pattern = vim.fn.getreg('/')
+        local current_time = vim.loop.hrtime() / 1e6
+
+        if current_pattern ~= last_pattern or (current ~= last_match_idx) then
+            last_pattern = current_pattern
+            last_match_idx = current
+            last_interaction_time = current_time
+        end
+
+        if (current_time - last_interaction_time) > timeout then
+            return true
+        end
+
+        return false
+    end
+
+    function M:update_status()
+        local ok, result = pcall(vim.fn.searchcount, { maxcount = self.options.maxcount, timeout = self.options.timeout })
+
+        if (not ok or next(result) == nil)
+        or (update_hl(result.current, self.options.display_timeout)) then
+            return ''
+        end
+
+        local denominator = math.min(result.total, result.maxcount)
+        if (result.current == 0 and result.total == 0) then
+            return '[no]'
+        elseif result.current == 0 and result.total ~= 0 then
+            return string.format('[n/%d]', denominator)
+        end
+
+        return string.format('[%d/%d]', result.current, denominator)
+    end
+
+    return M
+end
+
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -48,10 +102,10 @@ return {
                 lualine_a = { { 'mode', fmt = function(str) return str:sub(1,1) end } },
                 lualine_b = {'branch', 'diff', { 'diagnostics', icons_enabled = false, symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' }, } },
                 lualine_c = {{'filename', symbols = { unnamed = get_canola_dir() or '[No Name]' } } },
+                lualine_x = { search_count(), 'selectioncount', {'fileformat', icons_enabled = false, }, 'encoding', { 'filetype', icons_enabled = false, } },
                 lualine_y = {'progress'},
                 lualine_z = {'location'}
             },
-          lualine_x = {{'fileformat', icons_enabled = false, }, 'encoding', { 'filetype', icons_enabled = false, } },
             inactive_sections = {
                 lualine_a = {},
                 lualine_b = {},
